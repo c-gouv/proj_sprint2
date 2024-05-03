2212247ddaeacdd76c9db15d8567425317c0f043
João <joao.melo@sptech.school>
2024-05-03 13:48:35 -0300

    Banco de dados Atualizado com tabela medida adicionada

--- sql/Banco de Dados.sql
+++ sql/Banco de Dados.sql
@@ -2,10 +2,11 @@ CREATE DATABASE fitteya;
 
 USE fitteya;
 
+-- CRIAR UMA TABELA DE CADASTRO DE USUÁRIO/RESPONSÁVEL DA EMPRESA, ONDE 1 USUÁRIO PODE TER MAIS DE UM COMPLEXO
 CREATE TABLE usuario(
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45),
-    email VARCHAR(45),
+    nome VARCHAR(45) UNIQUE,
+    email VARCHAR(45) UNIQUE,
     telefone CHAR(11),
     senha VARCHAR(244),
     fkAdm INT,
@@ -18,8 +19,7 @@ INSERT INTO usuario VALUES
 	(default, 'Erick Gomes', 'erick@gmail.com', '11999999997', '4da77e6afb73aaaabd18cdfe8d3e0262', 1),
 	(default, 'Cauã Gouvea', 'gouvea@gmail.com', '11999999996', '0e3a650faff671a35b335526bae4aa05', 1);
 
-
-
+-- COMPLEXO - CONJUNTO DE SILOS
 CREATE TABLE complexo(
     idComplexo INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100),
@@ -37,23 +37,32 @@ VALUES  (default, 'TrigoBrasil',  'SP', 'São Paulo', 'Rua das Trigoiras', 123, 
         (default, 'GrãosPampas', 'RS', 'Caxias do Sul', 'Rua dos Grãos', 789, 3),
         (default, 'MoinhoReal', 'PR', 'Curitiba', 'Avenida do Moinho', 321, 4);
 
-CREATE TABLE silo ( 
-    idSilo INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45),
-    temperaturaMax DECIMAL(4, 2),
+CREATE TABLE medida(
+	idMedida INT PRIMARY KEY AUTO_INCREMENT,
     temperaturaMin DECIMAL(4, 2),
-    umidadeMax DECIMAL (4, 2),
+    temperaturaMax DECIMAL(4, 2),
     umidadeMin DECIMAL (4, 2),
+    umidadeMax DECIMAL (4, 2)
+);
+
+INSERT INTO medida
+VALUES 	(DEFAULT, 15, 18, 13, 16);
+
+CREATE TABLE silo ( 
+    idSilo INT AUTO_INCREMENT,
+    nome VARCHAR(45),
     fkComplexo INT,
-    CONSTRAINT fkComplexoSilo FOREIGN KEY (fkComplexo) REFERENCES complexo(idComplexo)
+    fkMedida INT,
+    CONSTRAINT fkComplexoSilo FOREIGN KEY (fkComplexo) REFERENCES complexo(idComplexo),
+    CONSTRAINT fkSiloMedida FOREIGN KEY (fkMedida) REFERENCES medida(idMedida),
+    CONSTRAINT pkSiloMedida PRIMARY KEY (idSilo, fkMedida)
 );
 
 INSERT INTO silo VALUES  
-	(default, 'SILO 1', 20.00, 00.00, 18.00, 00.00, 1),
-	(default, 'SILO 2', 15.00, 00.00, 13.00, 00.00, 2),
-	(default, 'SILO 3', 30.00, 00.00, 16.00, 00.00, 3),
-	(default, 'SILO 4', 20.00, 00.00, 10.00, 00.00, 4);
-
+	(default, 'SILO 1', 1, 1),
+	(default, 'SILO 2', 2, 1),
+	(default, 'SILO 3', 3, 1),
+	(default, 'SILO 4',  4, 1);
 
 CREATE TABLE sensor (
     idSensor INT PRIMARY KEY AUTO_INCREMENT,
@@ -68,31 +77,26 @@ VALUES  (default, 1),
         (default, 4);
 
 CREATE TABLE monitoramento ( 
-    idMonitoramento INT PRIMARY KEY AUTO_INCREMENT,
+    idMonitoramento INT AUTO_INCREMENT,
     temperatura DECIMAL(4,2),
     umidade DECIMAL(4,2),
     dataHora DATETIME,
     fkSensor INT,
-    CONSTRAINT fkSensorMonitoramento FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor) 
+    CONSTRAINT fkSensorMonitoramento FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor),
+    CONSTRAINT pkMonitoramentoSensor PRIMARY KEY (idMonitoramento, fkSensor)
 );
 
-CREATE TABLE historico(
-	idHistorico INT PRIMARY KEY AUTO_INCREMENT,
-    temperatura DECIMAL(4,2),
-    umidade DECIMAL(4,2),
-    dataHora DATETIME,
-    fkSensor INT,
-    CONSTRAINT fkSensorHistorico FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor) 
-);
+SELECT * FROM monitoramento;
 
 CREATE TABLE alerta(
-	idAlerta INT PRIMARY KEY AUTO_INCREMENT,
+	idAlerta INT AUTO_INCREMENT,
     motivo VARCHAR(100),
     dataHora DATETIME,
     temperaturaMomento DECIMAL(4,2),
     umidadeMomento DECIMAL(4,2),
     fkSensor INT,
-    CONSTRAINT fkSensorAlerta FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor)
+    CONSTRAINT fkSensorAlerta FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor),
+    CONSTRAINT pkAlertaSensor PRIMARY KEY (idAlerta, fkSensor)
 );
 
 INSERT INTO monitoramento
@@ -117,8 +121,6 @@ JOIN silo ON silo.fkComplexo = complexo.idComplexo
 JOIN sensor ON sensor.fkSilo = silo.idSilo
 JOIN monitoramento ON monitoramento.fkSensor = sensor.idSensor; 
         
-  
-        
 CREATE TABLE contatoSite(
     idContatoSite INT PRIMARY KEY AUTO_INCREMENT,
     empresa VARCHAR(100),
@@ -131,4 +133,3 @@ INSERT INTO contatoSite VALUES
 	(default, 'TrigoBrasil', 'contato@trigobrasil.com', 'Contrato', 'Gostaria de mais informações'),
 	(default, 'TrigoCeara', 'contato@trigoceara.com', 'Contrato', 'Gostaria de mais informações'),
 	(default, 'TrigoSãoPaulo', 'contato@trigosaopaulo.com', 'Contrato', 'Gostaria de mais informações');
-
